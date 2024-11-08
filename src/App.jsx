d684fdc5899b2c87dbd3cb60f388a01d0bf94dac
nebiy zeradawit <wedizere123@gmail.com>
2024-11-08 14:39:12 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Content from "./Content/content";
+
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-
+import Content from './Content/Content'
 
 
 function App() {
   return(
     <>
    <Header />
-   <Content />
+<Content />
    <Footer />
     </>
 
